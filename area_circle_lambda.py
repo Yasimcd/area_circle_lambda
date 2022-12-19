335eb1b6cce2ca8525b1274f81c8b7241c92dda8
Yasim <91527193+Yasimcd@users.noreply.github.com>
2022-12-18 18:22:42 -0800

    Add files via upload

---
+++ area_circle_lambda.py
@@ -0,0 +1,5 @@
+# Write a lambda function (area_circle_lambda) that returns the area of a circle of a given radius.
+from math import pi
+
+area_circle_lambda = lambda radius : pi * radius * radius
+
